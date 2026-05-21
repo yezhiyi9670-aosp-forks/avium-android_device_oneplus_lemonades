c416c23a54dac33d61adc7c43bc7b099c51fdc91
yezhiyi9670 <yezhiyi0562@foxmail.com>
2026-05-21 23:10:54 +0800

    Completely rework brightness config using the modern displayconfig way * Accomodate kernel exposure of HBM brightness levels * Make proper use of HBM (sunlight enhancement and HDR) * Re-tune auto brightness
    Change-Id: Ic433e9d3896542db838d72761d78ef151d3922cc

--- device.mk
+++ device.mk
@@ -27,6 +27,11 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     fstab.qcom.ramdisk
 
+# Display
+# The display ID can be obtained by observing output of `adb shell dumpsys display | grep local`
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/display/displayconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946950956839809.xml
+
 # LiveDisplay
 $(call soong_config_set_bool,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
 
